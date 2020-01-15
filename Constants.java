513c93d31f37a4207ae23fdc9239547884146524
Lena <43868081+LenaU748@users.noreply.github.com>
2020-01-15 16:04:27 -0600

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  //Drivebase Motor Controllers
+  public static final int LFF = 0;
+  public static final int LMF = 1;
+  public static final int LMB = 2;
+  public static final int LBB = 3;
+  public static final int RFF = 4;
+  public static final int RMF = 5;
+  public static final int RMB = 6;
+  public static final int RBB = 7;
+
+  public static final int kShooterAngleMotor = 8;
+  public static final int kShooterRPMMotor = 9;
+
+  public static final MotorType kMotorType = MotorType.kBrushless;
+
+  //Joystick Ports 
+  public static final int zero = 0,
+                          one = 1;
+
+  //Joystick Axes
+  public static final int XL = 0,
+                          YL = 1,
+                          XR = 4,
+                          YR = 5;
+
+
+}
