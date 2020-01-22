dbb95d8d9ec2569fc62263c1b5880cf49a848101
Lee-tao4 <56003364+Lee-tao4@users.noreply.github.com>
2020-01-22 17:47:12 -0600

    Add files via upload

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,15 +8,27 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.LidarBase;
 
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
-  //private Command m_autonomousCommand;
-  public static RobotContainer m_robotContainer;
+  private Command m_autonomousCommand;
+  public LidarBase m_LidarBase = new LidarBase();
 
-  private static final String k_path_name = "String";
-  
-  
+  private RobotContainer m_robotContainer;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
   @Override
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
@@ -56,31 +68,30 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-   /* m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
-    }*/
-    m_robotContainer.m_pathPlannerBase.pathSelector(k_path_name);
+    }
   }
 
-   
-    @Override
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
   public void autonomousPeriodic() {
-    
-    }
-
+  }
 
   @Override
   public void teleopInit() {
-    // This makes  sure that the autonomous stops running when
+    // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    /*if (m_autonomousCommand != null) {
+    if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
-    }*/
+    }
   }
 
   /**
@@ -88,7 +99,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    m_robotContainer.teleop();
+    SmartDashboard.putNumber("LIDAR distance Inches", m_LidarBase.getDistance());
   }
 
   @Override
